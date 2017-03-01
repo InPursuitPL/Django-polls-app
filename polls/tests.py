c8179f7d50a51b2d10a6d03397e905ed3798aca3
InPursuitPL <inpursuitpl@gmail.com>
2017-03-01 09:31:35 +0100

    Added index view improvement and test for it.

--- polls/tests.py
+++ polls/tests.py
@@ -6,9 +6,21 @@ from django.utils import timezone
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
-def create_question(question_text, days):
+def create_question_with_choice(question_text, days):
+    """
+    Creates a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    Also creates choice with this question.
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    q = Question.objects.create(question_text=question_text, pub_date=time)
+    q.choice_set.create(choice_text='Test choice', votes=0)
+    return q
+
+def create_question_no_choice(question_text, days):
     """
     Creates a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
@@ -33,7 +45,7 @@ class QuestionViewTests(TestCase):
         Questions with a pub_date in the past should be displayed on the
         index page.
         """
-        create_question(question_text="Past question.", days=-30)
+        create_question_with_choice(question_text="Past question.", days=-30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
@@ -45,7 +57,7 @@ class QuestionViewTests(TestCase):
         Questions with a pub_date in the future should not be displayed on
         the index page.
         """
-        create_question(question_text="Future question.", days=30)
+        create_question_with_choice(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
@@ -55,8 +67,8 @@ class QuestionViewTests(TestCase):
         Even if both past and future questions exist, only past questions
         should be displayed.
         """
-        create_question(question_text="Past question.", days=-30)
-        create_question(question_text="Future question.", days=30)
+        create_question_with_choice(question_text="Past question.", days=-30)
+        create_question_with_choice(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
@@ -67,14 +79,32 @@ class QuestionViewTests(TestCase):
         """
         The questions index page may display multiple questions.
         """
-        create_question(question_text="Past question 1.", days=-30)
-        create_question(question_text="Past question 2.", days=-5)
+        create_question_with_choice(question_text="Past question 1.", days=-30)
+        create_question_with_choice(question_text="Past question 2.", days=-5)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
 
+    def test_index_view_with_past_question_no_choices(self):
+        """
+        Question without choices should not be displayed.
+        """
+        create_question_no_choice(question_text="Past question.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+
+    def test_index_view_with_past_question_with_choice(self):
+        """
+        Question with choice should be displayed.
+        """
+        create_question_with_choice(question_text="Past question.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
 
 class QuestionMethodTests(TestCase):
 
@@ -112,7 +142,7 @@ class QuestionIndexDetailTests(TestCase):
         The detail view of a question with a pub_date in the future should
         return a 404 not found.
         """
-        future_question = create_question(question_text='Future question.', days=5)
+        future_question = create_question_with_choice(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
@@ -122,7 +152,7 @@ class QuestionIndexDetailTests(TestCase):
         The detail view of a question with a pub_date in the past should
         display the question's text.
         """
-        past_question = create_question(question_text='Past Question.', days=-5)
+        past_question = create_question_with_choice(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
@@ -133,7 +163,7 @@ class ResultsViewTests(TestCase):
         The result view of a question with a pub_date in the future should
         return a 404 not found.
         """
-        future_question = create_question(question_text='Future question.', days=5)
+        future_question = create_question_with_choice(question_text='Future question.', days=5)
         url = reverse('polls:results', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
--- polls/views.py
+++ polls/views.py
@@ -20,9 +20,11 @@ class IndexView(generic.ListView):
         Return the last five published questions (not including those set to be
         published in the future).
         """
+
         return Question.objects.filter(
-            pub_date__lte=timezone.now()
-            ).order_by('-pub_date')[:5]
+            pub_date__lte=timezone.now()).exclude(
+                choice__isnull=True).order_by(
+                    '-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
